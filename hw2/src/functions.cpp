3cb6b98245d5183c8a9036e6dc00d953edd5dc9c
Hubert King <hpk1@rice.edu>
2024-03-21 14:56:34 -0500

    column wise

--- hw2/src/functions.cpp
+++ hw2/src/functions.cpp
@@ -15,40 +15,27 @@ void back_solve_serial(double *A, double *b, double *x, int n){
 void back_solve_static(double *A, double *b, double *x, int n){
     omp_set_num_threads(NUM_THREADS);
     #pragma omp parallel for schedule(static)
-    {
-        for(int i = 0; i < n; ++i){
-        x[i] = b[i];
-    
-        }
+    for(int i = 0; i < n; ++i){
+    x[i] = b[i];
     }
     for (int j = n - 1; j >= 0; --j) {
         #pragma omp parallel for schedule(static)
-        {
-            for (int i = 0; i < j; ++i) {
-                x[i] -= A[i * n + j] * x[j];
-            }
+        for (int i = 0; i < j; ++i) {
+            x[i] -= A[i * n + j] * x[j];
         }
-
     }
 }
 
 
 void back_solve_dynamic(double *A, double *b, double *x, int n){
     omp_set_num_threads(NUM_THREADS);
-    #pragma omp parallel for schedule(dynamic)
-    {
-        for(int i = 0; i < n; ++i){
-        x[i] = b[i];
-    
-        }
+    for(int i = 0; i < n; ++i){
+    x[i] = b[i];
     }
     for (int j = n - 1; j >= 0; --j) {
         #pragma omp parallel for schedule(dynamic)
-        {
-            for (int i = 0; i < j; ++i) {
-                x[i] -= A[i * n + j] * x[j];
-            }
+        for (int i = 0; i < j; ++i) {
+            x[i] -= A[i * n + j] * x[j];
         }
-
     }
 }
