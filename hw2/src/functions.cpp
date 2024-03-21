0be92b4cd24d40c7e5efcd12c8efb7fb3b04f30d
Hubert King <hpk1@rice.edu>
2024-03-21 14:51:38 -0500

    column wise

--- hw2/src/functions.cpp
+++ hw2/src/functions.cpp
@@ -1,41 +1,54 @@
 #include "functions.h"
 #include <omp.h>
-
+void back_solve_serial(double *A, double *b, double *x, int n){
+    for(int i = 0; i < n; ++i){
+        x[i] = b[i];
+    }
+    
+    for (int j = n - 1; j >= 0; --j) {
+        for (int i = 0; i < j; ++i) {
+            x[i] -= A[i * n + j] * x[j];
+        }
+    }
+}
 
 void back_solve_static(double *A, double *b, double *x, int n){
     omp_set_num_threads(NUM_THREADS);
-    x[n-1] = b[n-1];
-    for (int i = n-2; i >= 0; --i) {
-        double sum = 0.0; 
-        #pragma omp parallel for schedule(static) reduction(+:sum)
-        for (int j = i+1; j < n; ++j) { 
-            sum += A[i*n+j] * x[j];
+    #pragma omp parallel for schedule(static)
+    {
+        for(int i = 0; i < n; ++i){
+        x[i] = b[i];
+    
         }
-        x[i] = b[i] - sum; 
+    }
+    for (int j = n - 1; j >= 0; --j) {
+        #pragma omp parallel for schedule(static)
+        {
+            for (int i = 0; i < j; ++i) {
+                x[i] -= A[i * n + j] * x[j];
+            }
+        }
+
     }
 }
 
 
 void back_solve_dynamic(double *A, double *b, double *x, int n){
     omp_set_num_threads(NUM_THREADS);
-    x[n-1] = b[n-1];
-    for (int i = n-2; i >= 0; --i) {
-        double sum = 0.0; 
-        #pragma omp parallel for schedule(dynamic) reduction(+:sum)
-        for (int j = i+1; j < n; ++j) { 
-            sum += A[i*n+j] * x[j];
+    #pragma omp parallel for schedule(dynamic)
+    {
+        for(int i = 0; i < n; ++i){
+        x[i] = b[i];
+    
         }
-        x[i] = b[i] - sum; 
     }
-}
-
-void back_solve_serial(double *A, double *b, double *x, int n){
-    x[n-1] = b[n-1];
-    for (int i = n-2; i >= 0; --i) {
-        double sum = 0.0; 
-        for (int j = i+1; j < n; ++j) { 
-            sum += A[i*n+j] * x[j];
+    for (int j = n - 1; j >= 0; --j) {
+        #pragma omp parallel for schedule(dynamic)
+        {
+            for (int i = 0; i < j; ++i) {
+                x[i] -= A[i * n + j] * x[j];
+            }
         }
-        x[i] = b[i] - sum; 
+
     }
 }
