23f15820759a5b3b59746af5ab6ed8b07a391849
Hubert King <hpk1@rice.edu>
2024-03-21 20:02:21 -0500

    column wise

--- hw2/src/functions.cpp
+++ hw2/src/functions.cpp
@@ -29,6 +29,7 @@ void back_solve_static(double *A, double *b, double *x, int n){
 
 void back_solve_dynamic(double *A, double *b, double *x, int n){
     omp_set_num_threads(NUM_THREADS);
+    #pragma omp parallel for schedule(dynamic)
     for(int i = 0; i < n; ++i){
     x[i] = b[i];
     }
