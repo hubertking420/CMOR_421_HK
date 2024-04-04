9d976f5d30be7f4e6f6584dbe0e391b6b2632696
Hubert King <hpk1@rice.edu>
2024-04-03 21:19:59 -0500

    cannon

--- hw3/cannon.cpp
+++ hw3/cannon.cpp
@@ -14,21 +14,23 @@ int main(int argc, char * argv[]){
     MPI_Comm_size(MPI_COMM_WORLD, &size); 
     MPI_Status status;
     MPI_Barrier(MPI_COMM_WORLD);
-    if(rank == 0) {
-        double * A = new double[n * n];
-        double * B = new double[n * n];
-        for (int i = 0; i < n * n; ++i){
-            A[i] = 1.0;
-            B[i] = 1.0;
-        }
+
+    // Construct system (make this function param)
+    double * A = new double[n * n];
+    double * B = new double[n * n];
+    for (int i = 0; i < n * n; ++i){
+        A[i] = 1.0;
+        B[i] = 1.0;
     }
 
     if(rank == 0){
         cout << "Matrix size n = " << n << endl;
     }
 
-    // Dimensions of mesh
+    // Start time
     double start = MPI_Wtime();
+
+    // Dimensions of mesh
     int s = size;
     int p = (int)sqrt(s);
     int block_size = n / p;
@@ -37,6 +39,7 @@ int main(int argc, char * argv[]){
     double * A_ij = new double[block_size * block_size];
     double * B_ij = new double[block_size * block_size];
     double * C_ij = new double[block_size * block_size];
+
     // Construct system on root rank
     if (rank == 0) {
         for (int k = size-1; k > 0; --k) {
@@ -59,36 +62,45 @@ int main(int argc, char * argv[]){
             } 
         } 
     } else {
-        // All other processes receive
+        // All other processes receive blocks
         MPI_Recv(A_ij, block_size*block_size, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
         MPI_Recv(B_ij, block_size*block_size, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
     }
 
+    // Progress check
     MPI_Barrier(MPI_COMM_WORLD);
-    cout << "Paritions recieved on rank: " << rank << endl;
+    bool check_paritions_recieved = false;
+    if(check_paritions_recieved){
+        cout << "Paritions recieved on rank: " << rank << endl;
+    }
 
     // Map 2D coordinates (x, y) to 1D processor labels
     int x = rank / p;
     int y = rank % p;
     // Initial skew for A
-    // Shift each row of A left by 'x' positions
     int origin_A = x * p + (y - x + p) % p; // Source rank for A submatrix
     int dest_A = x * p + (y + x) % p;       // Destination rank for A submatrix
     MPI_Sendrecv_replace(A_ij, block_size*block_size, MPI_DOUBLE, dest_A, 0,
                         origin_A, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
     // Initial skew for B
-    // Shift each column of B up by 'y' positions
     int origin_B = ((x - y + p) % p) * p + y; // Source rank for B submatrix
     int dest_B = ((x + y) % p) * p + y;       // Destination rank for B submatrix
     MPI_Sendrecv_replace(B_ij, block_size*block_size, MPI_DOUBLE, dest_B, 0,
                         origin_B, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+
+    // Progress check
     MPI_Barrier(MPI_COMM_WORLD);
-    cout << "Inital skew of A and B completed on rank: " << rank << endl;
+    bool check_initial_skew = false;
+    if(check_initial_skew){
+        cout << "Inital skew of A and B completed on rank: " << rank << endl;
+    }
+
 
     // Main computational loop
     for(int k = 0; k < p; ++k){ 
-        // Accumulation
+        // Accumulation into C
         for(int i = 0; i < block_size; ++i){
             for(int j = 0; j < block_size; ++j){
                 for(int k = 0; k < block_size; ++k){
@@ -105,14 +117,18 @@ int main(int argc, char * argv[]){
         MPI_Sendrecv_replace(A_ij, block_size*block_size, MPI_DOUBLE, dest_A, 0, origin_A, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         MPI_Sendrecv_replace(B_ij, block_size*block_size, MPI_DOUBLE, dest_B, 0, origin_B, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
     }
+
+
+    // Progress check
     MPI_Barrier(MPI_COMM_WORLD);
-    cout << "Paritions of C computed on rank: " << rank << endl;
- 
+    bool check_paritions_computed = true;
+    if(check_paritions_computed){
+        cout << "Partitions of C computed on rank: " << rank << endl;
+    }
+
 
     // Gather blocks of C
     double *C = nullptr;
-    MPI_Barrier(MPI_COMM_WORLD);
-    cout << "Computation completed on rank: " << rank << endl;
     if(rank == 0) {
         // Allocate space for the full matrix on rank 0
         C = new double[n * n];
@@ -141,15 +157,22 @@ int main(int argc, char * argv[]){
         // Non-root ranks send their C_ij block to rank 0
         MPI_Send(C_ij, block_size*block_size, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
     }
-    MPI_Barrier(MPI_COMM_WORLD);
-    cout << "Paritions written into C from rank = " << rank << endl;
 
+
+    // Progress check
+    MPI_Barrier(MPI_COMM_WORLD);
+    bool check_gather = false;
+    if(check_gather){
+        cout << "Paritions written into C from rank = " << rank << endl;
+    }
+
+
+    // Stop clock
     double elapsed = MPI_Wtime()-start;
     
-    
-    bool display_A = true;
+    // Display the matrix A
+    bool display_A = false;
     if(rank == 0){
-        // Display the matrix C
         if(display_A){
             cout << "Matrix A:" << endl;
             for (int i = 0; i < n * n; ++i){
@@ -160,9 +183,9 @@ int main(int argc, char * argv[]){
             }
         }
     }
-    bool display_B = true;
+   // Display the matrix B
+   bool display_B = false;
     if(rank == 0){
-        // Display the matrix C
         if(display_B){
             cout << "Matrix B:" << endl;
             for (int i = 0; i < n * n; ++i){
@@ -173,12 +196,11 @@ int main(int argc, char * argv[]){
             }
         }
     }
-    
+    // Display the matrix C
     bool display_C = true;
     if(rank == 0){
         // Display the matrix C
         cout << "Matrix C:" << endl;
-
         if(display_C){
             for (int i = 0; i < n * n; ++i){
                 cout << C[i] << " ";
@@ -195,11 +217,12 @@ int main(int argc, char * argv[]){
         delete[] C;
         cout << "Elapsed time: " << elapsed << endl;
     }
-    // Clear local memory
+
+
+    // Clear memory
     delete[] A_ij;
     delete[] B_ij;
     delete[] C_ij; 
     MPI_Finalize();
-
 
 }
