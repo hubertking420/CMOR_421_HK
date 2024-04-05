2537d8329cebd4b8ecdb8934fb7ecf5603b0bae1
Hubert King <hpk1@rice.edu>
2024-04-05 10:31:13 -0500

    m

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -16,24 +16,25 @@ int main(int arc, char* argv[]){
 
     int n = atoi(argv[1]);
     int num_trials = 5;
-    cout << "Matrix size n = " << n << endl;
-
     if(rank == 0){
-        // make A, B = random
-        double * A = new double[n * n];
-        double * B = new double[n * n];
-        double * C = new double[n * n];    
-        random_device rd;
-        default_random_engine eng(rd());
-        uniform_real_distribution<double> distr(0.0, 10.0);
-        for (int i = 0; i < n; ++i){
-            A[i + i * n] = distr(eng);
-            B[i + i * n] = distr(eng);
-        }
-        for (int i = 0; i < n * n; ++i){
-            C[i] = 0.0;
-        } 
+        cout << "Matrix size n = " << n << endl;
     }
+
+    // make A, B = random
+    double * A = new double[n * n];
+    double * B = new double[n * n];
+    double * C = new double[n * n];    
+    random_device rd;
+    default_random_engine eng(rd());
+    uniform_real_distribution<double> distr(0.0, 10.0);
+    for (int i = 0; i < n; ++i){
+        A[i + i * n] = distr(eng);
+        B[i + i * n] = distr(eng);
+    }
+    for (int i = 0; i < n * n; ++i){
+        C[i] = 0.0;
+    } 
+
 
 
     bool verbose = false;
@@ -43,8 +44,8 @@ int main(int arc, char* argv[]){
    
     
     // Serial
-    matmul_naive(n, C, A, B);
     if(rank == 0){
+        matmul_naive(n, C, A, B);
         reset(n, C);
     }
 
